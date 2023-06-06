7138b639faf3af373d1da7d0160f14844dd441ca
chavonne-kek <22017979@myrp.edu.sg>
2023-06-06 12:36:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22017979/demodynamic/MainActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.rp.c346.id22017979.demodynamic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etElement, etIndexElement;
+    Button btnAdd;
+    ListView lvColor;
+    ArrayList<String> alColors;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        alColors = new ArrayList<String>();
+        etElement = findViewById(R.id.editTextColor);
+        btnAdd = findViewById(R.id.btnAddColor);
+        lvColor = findViewById(R.id.listViewColor);
+        etIndexElement = findViewById(R.id.editTextIndex);
+
+        alColors.add("Red");
+        alColors.add("Orange");
+        ArrayAdapter aaColor = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alColors);
+        lvColor.setAdapter(aaColor);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newColor = etElement.getText().toString();
+                //alColors.add(newColor);
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColors.add(pos,newColor);
+                aaColor.notifyDataSetChanged();
+                etElement.setText("");
+                etIndexElement.setText("");
+            }
+        });
+        lvColor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String color = alColors.get(position);
+
+                Toast.makeText(MainActivity.this, "Color selected: "+color, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
